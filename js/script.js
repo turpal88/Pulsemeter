919c5aca6fc222fb83b38f74054bcc7148ed9742
Ivan Buldakov <pereslav88@gmail.com>
2020-11-19 20:04:52 +0300

    tiny-slider

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
-$(document).ready(function(){
+// $(document).ready(function(){
     
-$('.carousel__inner').slick({
+// $('.carousel__inner').slick({
 
-adaptiveHeight: true,
-speed: 600,
-prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.svg" alt="arrow"></button>',
-nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.svg" alt="arrow"></button>',
-responsive: [
-    {
-        breakpoint: 575,
-        settings: {
-        arrows: false,
-        dots: true
-    }
-    }
+// adaptiveHeight: true,
+// speed: 600,
+// prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.svg" alt="arrow"></button>',
+// nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.svg" alt="arrow"></button>',
+// responsive: [
+//     {
+//         breakpoint: 575,
+//         settings: {
+//         arrows: false,
+//         dots: true
+//     }
+//     }
     
-]
+// ]
        
         
-    });
-  });
+//     });
+//   });
   
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    autoHeight: true
+    
+    
+  });
+  document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
+  document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+  };
