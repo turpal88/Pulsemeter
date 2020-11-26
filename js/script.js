b7aa3b025df0ce8769b2994c61824a2b822b7d10
Ivan Buldakov <pereslav88@gmail.com>
2020-11-26 15:52:11 +0300

    add tabs, feedbacks

--- js/script.js
+++ js/script.js
@@ -19,5 +19,25 @@ responsive: [
        
         
     });
-  });
-  
+
+$('ul.catalog-tabs').on('click', 'li:not(.active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+function switchClass(item){
+    $(item).each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.item__main-content').eq(i).toggleClass('item__main-content_active');
+            $('.catalog__addition').eq(i).toggleClass('catalog__addition_active');
+        });
+    });
+}
+switchClass('.catalog-item__link');
+switchClass('.catalog-item__back');
+
+});
+
+
